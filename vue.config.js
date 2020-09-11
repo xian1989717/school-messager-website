183474f85de98fbc6af03ce841a418b35b1a2f14
刘述 <383883040@qq.com>
2020-09-11 18:33:32 +0800

    提交代码

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pages: {
+    about: {
+      entry: 'src/pages/about/main.js',
+      template: 'public/about.html',
+      filename: 'about.html'
+    },
+    index: {
+      entry: 'src/pages/home/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  }
+}
