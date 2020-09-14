8609dde2c9c58be229e1df8796245c59cc37cd9e
刘述 <383883040>
2020-09-14 21:55:35 +0800

    提交代码

--- src/common/home.vue
+++ src/common/home.vue
@@ -40,8 +40,6 @@
   .el-main {
     background-color: #e9eef3;
     color: #333;
-    text-align: center;
-    line-height: 160px;
   }
 
   body > .el-container {
--- src/components/grade-group/detail.vue
+++ src/components/grade-group/detail.vue
@@ -1,5 +1,42 @@
 <template>
   <div>
-    1234
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">详情</a></el-breadcrumb-item>
+    </el-breadcrumb>
+    <div
+      class="detail-header">
+      <b style="font-size:24px;">
+        罗老师
+      </b>
+      <div>
+        <el-button>编辑</el-button>
+        <el-button>删除</el-button>
+      </div>
+    </div>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <div class="grid-content bg-purple">1234</div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple">1234</div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple">134</div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple">1234</div>
+      </el-col>
+    </el-row>
   </div>
 </template>
+
+<style scoped>
+  .detail-header {
+    display: flex;
+    justify-content: space-between;
+    margin: 16px 0px;
+    height: 40px;
+    line-height: 40px;
+  }
+</style>
